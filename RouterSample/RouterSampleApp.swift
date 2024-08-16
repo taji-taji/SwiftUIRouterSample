86c49e37ddb40d4d519bca281abd22dc54dc0b2d
taji-taji <y.tajika529@gmail.com>
2024-08-16 21:36:43 +0900

    Initial Commit

---
+++ RouterSample/RouterSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RouterSampleApp.swift
+//  RouterSample
+//
+//  Created by 多鹿豊 on 2024/08/16.
+//
+
+import SwiftUI
+
+@main
+struct RouterSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
